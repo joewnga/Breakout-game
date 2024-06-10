6bcd57b02e07966d57dd90776c6037706e2f2f5d
joewnga <66113453+joewnga@users.noreply.github.com>
2024-06-10 14:37:05 +0300

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# Breakout-game
+# Breakout Game
+
+Breakout Game is a classic arcade game built with Rust and the `macroquad` game development framework. Its a simple game created using Rust, Docker, and CI/CD with GitHub Actions.
+
+![Breakout Game](assets/images/breakout_game.png)
+
+## Overview
+
+In this Breakout Game, players control a paddle to bounce a ball against a set of bricks. The goal is to break all the bricks without letting the ball fall off the screen. The game includes features such as:
+
+- Player-controlled paddle
+- Ball movement and collision detection
+- Multiple layers of bricks with varying colors
+- Responsive game loop
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Acknowledgments](#acknowledgments)
+
+## Installation
+
+### Prerequisites
+
+- [Docker](https://www.docker.com/get-started)
+- [Rust](https://www.rust-lang.org/tools/install)
+
+
+### Running Locally
+
+1. Clone the repository:
+
+    ```sh
+    git clone https://github.com/joewnga/Breakout-game.git
+    cd Breakout-game
+    ```
+
+2. Install Rust and Cargo if you haven't already. Link [here](https://www.rust-lang.org/tools/install)
+
+
+3. Build and run the project:
+
+    ```sh
+    cargo run
+    ```
+
+
+### Running with Docker
+
+1. Clone the repository:
+
+    ```sh
+    git clone https://github.com/joewnga/Breakout-game.git
+    cd Breakout-game
+    ```
+
+2. Build and run the project using Docker:
+
+    ```sh
+    docker build -t Breakout-game .
+    docker run -it --rm -p 8000:8000 Breakout-game
+    ```
+
+3. Open the project in Visual Studio Code:
+
+    ```sh
+    code .
+    ```
+
+4. Open the project in a Dev Container.
+
+5. Run the game:
+
+    ```sh
+    cargo run
+    ```
+
+## Usage
+
+Use the left and right arrow keys to move the paddle and keep the ball in play. Break all the bricks to win the game!
+
+
+
+## Tests
+
+Run unit and integration tests using the following command:
+
+```sh
+cargo test
+```
+
+## Acknowledgments
+
+This project draws inspiration from and includes code from the [TanTanDev](https://github.com/TanTanDev/breakout_tutorial) repository.
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,57 +4,6 @@ use breakout_game::ball::{Ball, Circle, BALL_RADIUS, BALL_SPEED};
 use breakout_game::utils::init_blocks;
 use macroquad::prelude::*;
 
-#[test]
-fn game_initialization() {
-    
-    const MOCK_SCREEN_WIDTH: f32 = 800.0;
-    const MOCK_SCREEN_HEIGHT: f32 = 600.0;
-
-    let player = Player {
-        rect: Rect::new(
-            MOCK_SCREEN_WIDTH * 0.5 - PLAYER_SIZE.x * 0.5,
-            MOCK_SCREEN_HEIGHT - 100.0,
-            PLAYER_SIZE.x,
-            PLAYER_SIZE.y,
-        ),
-    };
-    assert!(player.rect.x >= 0.0);
-
-    let ball = Ball {
-        circle: Circle::new(vec2(50.0, 50.0), BALL_RADIUS),
-        vel: vec2(1.0, 1.0).normalize(),
-    };
-    assert_eq!(ball.circle.center.x, 50.0);
-    assert_eq!(ball.circle.center.y, 50.0);
-
-    let mut blocks = Vec::new();
-    init_blocks(&mut blocks);
-    assert!(!blocks.is_empty());
-}
-
-#[test]
-fn player_movement() {
-    const MOCK_SCREEN_WIDTH: f32 = 800.0;
-    const MOCK_SCREEN_HEIGHT: f32 = 600.0;
-
-    // Testing player movement
-    let mut player = Player {
-        rect: Rect::new(
-            MOCK_SCREEN_WIDTH * 0.5 - PLAYER_SIZE.x * 0.5,
-            MOCK_SCREEN_HEIGHT - 100.0,
-            PLAYER_SIZE.x,
-            PLAYER_SIZE.y,
-        ),
-    };
-
-    // Simulate movement to the right
-    let initial_x = player.rect.x;
-    player.rect.x += PLAYER_SPEED * 1.0;
-
-    assert!(player.rect.x >= 0.0 && player.rect.x + player.rect.w <= MOCK_SCREEN_WIDTH);
-    assert!(player.rect.x > initial_x);
-}
-
 //test ball movement
 #[test]
 fn ball_movement() {
@@ -67,3 +16,56 @@ fn ball_movement() {
 
     assert!(ball.circle.center.x != 50.0 || ball.circle.center.y != 50.0);
 }
+
+// #[test]
+// fn game_initialization() {
+    
+//     const MOCK_SCREEN_WIDTH: f32 = 800.0;
+//     const MOCK_SCREEN_HEIGHT: f32 = 600.0;
+
+//     let player = Player {
+//         rect: Rect::new(
+//             MOCK_SCREEN_WIDTH * 0.5 - PLAYER_SIZE.x * 0.5,
+//             MOCK_SCREEN_HEIGHT - 100.0,
+//             PLAYER_SIZE.x,
+//             PLAYER_SIZE.y,
+//         ),
+//     };
+//     assert!(player.rect.x >= 0.0);
+
+//     let ball = Ball {
+//         circle: Circle::new(vec2(50.0, 50.0), BALL_RADIUS),
+//         vel: vec2(1.0, 1.0).normalize(),
+//     };
+//     assert_eq!(ball.circle.center.x, 50.0);
+//     assert_eq!(ball.circle.center.y, 50.0);
+
+//     let mut blocks = Vec::new();
+//     init_blocks(&mut blocks);
+//     assert!(!blocks.is_empty());
+// }
+
+// #[test]
+// fn player_movement() {
+//     const MOCK_SCREEN_WIDTH: f32 = 800.0;
+//     const MOCK_SCREEN_HEIGHT: f32 = 600.0;
+
+//     // Testing player movement
+//     let mut player = Player {
+//         rect: Rect::new(
+//             MOCK_SCREEN_WIDTH * 0.5 - PLAYER_SIZE.x * 0.5,
+//             MOCK_SCREEN_HEIGHT - 100.0,
+//             PLAYER_SIZE.x,
+//             PLAYER_SIZE.y,
+//         ),
+//     };
+
+//     // Simulate movement to the right
+//     let initial_x = player.rect.x;
+//     player.rect.x += PLAYER_SPEED * 1.0;
+
+//     assert!(player.rect.x >= 0.0 && player.rect.x + player.rect.w <= MOCK_SCREEN_WIDTH);
+//     assert!(player.rect.x > initial_x);
+// }
+
+
